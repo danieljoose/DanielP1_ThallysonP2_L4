f2e5d6c30fc86c6c4335aa89d268a7c9e748a814
Daniel José <daniel.jose.leite@ccc.ufcg.edu.br>
2017-06-29 11:59:53 -0300

    Classe Recepção

---
+++ src/hotel/Recepção.java
@@ -0,0 +1,14 @@
+package hotel;
+
+public class Recepção {
+
+	private ArrayList <Estadia> estadias = new ArrayList<>();
+	
+	public void checkIn(String nome, String tipo, int idade, int dias, double valor) {
+		Animal animal = new Animal();
+	
+		
+	}
+	
+	
+}
